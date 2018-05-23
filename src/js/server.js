350d3f461dc0e18449aaf53080e4ebb1672efade
omri wallach <omriwallach1992@gmail.com>
2018-05-23 19:46:37 +0000

    add the webpack config to the server

--- src/js/server.js
+++ src/js/server.js
@@ -1,2 +1,18 @@
 var express = require("express");
 var server = express();
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const config = require('../../webpack.config.js');
+const compiler = webpack(config);
+
+server.use(webpackDevMiddleware(compiler,{
+    publicPath: config.output.publicPath
+}));
+
+server.get('/',function(req,res){
+    res.send('work');
+});
+
+server.listen(process.env.PORT,function(){
+    console.log('server is up');
+});
