941c5d24ddcc6abd91b67d46e01ef2ca325ece6a
omri wallach <omriwallach1992@gmail.com>
2018-05-29 10:39:44 +0000

    add charts, chartpanel

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React,{Component} from 'react';
 import Header from './header.js';
 import Footer from './footer.js';
+import ChartPanel from './charts/panel.js';
 
 export default class App extends Component {
     render(){
         return(
             <div id="app">
                 <Header/>
+                <ChartPanel/>
                 <Footer/>
             </div>
         );
