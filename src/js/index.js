365743591800a9be0d73eff63ee8615ebfc81abb
omri wallach <omriwallach1992@gmail.com>
2018-05-31 13:11:37 +0000

    add font-awesome

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import App from '../components/app.js';
 import ReactDom from 'react-dom';
 import React from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/font-awesome/css/font-awesome.min.css';
 
 
 
