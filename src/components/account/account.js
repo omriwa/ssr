ddd569581ca9055003e46cc137c100a64bda15b1
omri wallach <omriwallach1992@gmail.com>
2018-06-07 20:16:03 +0000

    add front end call to account routes, need to fix it

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -1,8 +1,18 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+
 
 class RegisterPanel extends Component {
-    render(){
-        return(
+    constructor(props) {
+        super(props);
+        this.postData = this.postData.bind(this);
+    }
+
+    postData() {
+        alert('postData')
+    }
+
+    render() {
+        return (
             <form className={"account-form"} id="register">
                 <label>First name:</label>
                 <input type="text"/>
@@ -23,34 +33,47 @@ class RegisterPanel extends Component {
 }
 
 class LoginPanel extends Component {
-    render(){
-        return(
+    constructor(props) {
+        super(props);
+        this.postData = this.postData.bind(this);
+    }
+
+    postData(e) {
+        fetch('https://ssr-omriwallach.c9users.io/account/login', {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: {}
+        });
+    }
+
+    render() {
+        return (
             <form className={"account-form"} id="login">
                 <label>Username:</label>
                 <input type="text"/>
                 <label>Password:</label>
                 <input type="passowrd"/>
-                <button className={"btn btn-md btn-primary"}>Login</button>
+                <button className={"btn btn-md btn-primary"} onClick={this.postData}>Login</button>
             </form>
         );
     }
 }
 
 export default class AccountPanel extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {login: true};
+        this.state = { login: true };
     }
-    
-    renderLoginOrRegister(){
-        if(this.state.login)
+
+    renderLoginOrRegister() {
+        if (this.state.login)
             return <LoginPanel/>;
         else
             return <RegisterPanel/>;
     }
-    
-    render(){
-        return(
+
+    render() {
+        return (
             <div id="account-panel">
                 {this.renderLoginOrRegister()}
                 <button onClick={() => {this.setState({login: false})}}>Register</button>
