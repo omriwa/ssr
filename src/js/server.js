c4f1097eaae438f887671a591930085aaf55677d
omri wallach <omriwallach1992@gmail.com>
2018-06-08 11:54:09 +0000

    add mongodb and mongoose, server entry point

--- src/js/server.js
+++ src/js/server.js
@@ -8,22 +8,30 @@ const path = require("path");
 const apiRouter = require("./api.js");
 const accountRouter = require("./accountRoutes.js");
 const bodyParser = require("body-parser");
+const MongoClient = require('mongodb').MongoClient;
 
 
 // CONFIGURATION
-server.use(webpackDevMiddleware(compiler,{
+server.use(webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath
 }));
 
-server.use(express.static(path.join(__dirname,'dist')));
+server.use(express.static(path.join(__dirname, 'dist')));
 // ROUTES
-server.get('/',function(req,res){
+server.get('/', function(req, res) {
     res.render('index.html');
 });
-server.use('/api',apiRouter);
-server.use('/account',accountRouter);
+server.use('/api', apiRouter);
+server.use('/account', accountRouter);
+// DB
+MongoClient.connect(process.env.IP, function(err, client) {
+    if(err)
+        console.log('DB is disconnected');
+    else
+        console.log('DB is connected');
+
+});
 // LISTEN
-server.listen(process.env.PORT,function(){
+server.listen(process.env.PORT, function() {
     console.log('server is up');
 });
-
